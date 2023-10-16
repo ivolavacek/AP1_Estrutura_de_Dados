234e1d4fa3ad413bf03deb134f57593b89cbd654
ivolavacek <ivo.lavacek@gmail.com>
2023-10-16 16:33:54 -0300

    indentação main

--- codigo/main.go
+++ codigo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "bufio"
-  "os"
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -22,13 +22,13 @@ func exibirMenu() {
 	fmt.Println("3. Buscar produto por ID")
 	fmt.Println("4. Exibir todos os produtos")
 	fmt.Println("5. Adicionar pedido")
-  fmt.Println("6. Exibir pedidos em andamento")
-  fmt.Println("7. Exibir pedidos por ID")
+	fmt.Println("6. Exibir pedidos em andamento")
+	fmt.Println("7. Exibir pedidos por ID")
 	fmt.Println("8. Expedir pedidos")
 	fmt.Println("9. Exibir métricas")
 	fmt.Println("10. Sair")
 	fmt.Println("==========================")
-  fmt.Println("")
+	fmt.Println("")
 	fmt.Println("Digite sua opção:")
 }
 
@@ -49,14 +49,14 @@ func tratarEscolha(escolha int) {
 	case 4:
 		exibirProdutos()
 	case 5:
-    fmt.Println("Cardápio:")
-    exibirProdutos()
-    fmt.Println("")
+		fmt.Println("Cardápio:")
+		exibirProdutos()
+		fmt.Println("")
 		adicionarPedidoInterface()
 	case 6:
 		exibirPedidosEmAndamento()
-  case 7:
-    exibirPedidosPorIDInterface()
+	case 7:
+		exibirPedidosPorIDInterface()
 	case 8:
 		expedirPedidoInterface()
 	case 9:
@@ -77,7 +77,7 @@ func cadastrarProdutos() {
 
 	var produtosLote []Produto
 
-  scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for i := 0; i < numeroProdutos; i++ {
 		var nome, descricao string
@@ -85,11 +85,11 @@ func cadastrarProdutos() {
 
 		fmt.Printf("Digite o nome do produto %d:\n", i+1)
 		scanner.Scan()
-    nome = scanner.Text()
+		nome = scanner.Text()
 
 		fmt.Printf("Digite a descrição do produto %d:\n", i+1)
 		scanner.Scan()
-    descricao = scanner.Text()
+		descricao = scanner.Text()
 
 		fmt.Printf("Digite o valor do produto %d:\n", i+1)
 		fmt.Scan(&valor)
@@ -107,10 +107,10 @@ func cadastrarProdutos() {
 
 func interfaceRemoverProduto() {
 	var id int
-  
+
 	fmt.Println("Digite o ID do produto a ser removido:")
 	fmt.Scan(&id)
-	removerProduto(id)  // chamando a função do arquivo produto.go
+	removerProduto(id) // chamando a função do arquivo produto.go
 }
 
 func buscarProdutoPorIDInterface() {
@@ -126,63 +126,63 @@ func buscarProdutoPorIDInterface() {
 }
 
 func adicionarPedidoInterface() {
-  var entrega bool
-  var opcaoEntrega string
-  produtosPedido := []ProdutoPedido{} // Inicializa uma lista de produtos vazia
+	var entrega bool
+	var opcaoEntrega string
+	produtosPedido := []ProdutoPedido{} // Inicializa uma lista de produtos vazia
 
-  for {
-    var idProduto, quantidade int
-    fmt.Println("Digite o ID do produto (ou digite 0 para encerrar a seleção):")
-    fmt.Scan(&idProduto)
+	for {
+		var idProduto, quantidade int
+		fmt.Println("Digite o ID do produto (ou digite 0 para encerrar a seleção):")
+		fmt.Scan(&idProduto)
 
-    if idProduto == 0 {
-      break // Encerra a seleção de produtos
-    }
+		if idProduto == 0 {
+			break // Encerra a seleção de produtos
+		}
 
-    produto := buscarProdutoPorID(idProduto)
-    if produto == nil {
-      fmt.Println("Produto não encontrado. Por favor, tente novamente.")
-      continue // Volta para a próxima iteração
-    }
+		produto := buscarProdutoPorID(idProduto)
+		if produto == nil {
+			fmt.Println("Produto não encontrado. Por favor, tente novamente.")
+			continue // Volta para a próxima iteração
+		}
 
-    fmt.Println("Digite a quantidade do produto:")
-    fmt.Scan(&quantidade)
+		fmt.Println("Digite a quantidade do produto:")
+		fmt.Scan(&quantidade)
 
-    produtosPedido = append(produtosPedido, ProdutoPedido{
-      Produto:   *produto,
-      Quantidade: quantidade,
-    })
-  }
+		produtosPedido = append(produtosPedido, ProdutoPedido{
+			Produto:    *produto,
+			Quantidade: quantidade,
+		})
+	}
 
-  fmt.Println("É um pedido de entrega? (s/n)")
-  fmt.Scan(&opcaoEntrega)
-  if strings.ToLower(opcaoEntrega) == "s" {
-    entrega = true
-  }
+	fmt.Println("É um pedido de entrega? (s/n)")
+	fmt.Scan(&opcaoEntrega)
+	if strings.ToLower(opcaoEntrega) == "s" {
+		entrega = true
+	}
 
-  adicionarPedido(entrega, produtosPedido)
+	adicionarPedido(entrega, produtosPedido)
 }
 
 func exibirPedidosPorIDInterface() {
-  var id int
-  fmt.Println("Digite o ID do pedido a ser buscado:")
-  fmt.Scan(&id)
-  pedido := exibirPedidosPorID(id)
-  if pedido != nil {
-    fmt.Println("")
-    for _, produto := range pedido.Produtos {
-      fmt.Printf("%d %s\n", produto.Quantidade, produto.Produto.Nome)
-    }
-    if pedido.Entrega {
-      fmt.Println("Delivery")
-    }
-    fmt.Printf("Valor total: R$%.2f\nCriado em: %s\n", pedido.ValorTotal, pedido.CriadoEm.Format("02/01/2006 15:04:05"))
-    if !pedido.ExpedidoEm.IsZero() {
-      fmt.Printf("Expedido em: %s\n", pedido.ExpedidoEm.Format("02/01/2006 15:04:05"))
-    }
-  } else {
-    fmt.Println("Pedido não encontrado.")
-  }
+	var id int
+	fmt.Println("Digite o ID do pedido a ser buscado:")
+	fmt.Scan(&id)
+	pedido := exibirPedidosPorID(id)
+	if pedido != nil {
+		fmt.Println("")
+		for _, produto := range pedido.Produtos {
+			fmt.Printf("%d %s\n", produto.Quantidade, produto.Produto.Nome)
+		}
+		if pedido.Entrega {
+			fmt.Println("Delivery")
+		}
+		fmt.Printf("Valor total: R$%.2f\nCriado em: %s\n", pedido.ValorTotal, pedido.CriadoEm.Format("02/01/2006 15:04:05"))
+		if !pedido.ExpedidoEm.IsZero() {
+			fmt.Printf("Expedido em: %s\n", pedido.ExpedidoEm.Format("02/01/2006 15:04:05"))
+		}
+	} else {
+		fmt.Println("Pedido não encontrado.")
+	}
 }
 
 func expedirPedidoInterface() {
